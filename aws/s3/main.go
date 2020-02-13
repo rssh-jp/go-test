fd599cc45e5b7cd1d911da6b9b943750ab673cc9
ARAUMI Teruo <araumi@gnavi.co.jp>
2020-02-13 16:19:28 +0900

    commit

--- aws/s3/main.go
+++ aws/s3/main.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"io"
 	"log"
-    "io"
 	"os"
 	"time"
-    "net/http"
+	//"net"
+	"net/http"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/s3"
+	//"github.com/aws/aws-sdk-go/service/s3/s3manager"
 
-    "github.com/rssh-jp/go-bandwidth"
+	"github.com/rssh-jp/go-bandwidth"
 )
 
 const (
 	filepath = "/opt/rsrc/giga1"
+	//filepath = "/opt/rsrc/halfgiga1"
 	//filepath = "/opt/rsrc/mega1"
 	//filepath = "/opt/rsrc/byte100"
 )
@@ -24,30 +27,33 @@ func measuretime(t time.Time) {
 	log.Println(time.Now().Sub(t))
 }
 
-type MyTransport struct{
-    http.Transport
+type MyTransport struct {
+	http.Transport
 }
 
-type ReadCloser struct{
-    *bandwidth.ReadWriter
-    io.Closer
+type ReadCloser struct {
+	*bandwidth.ReadWriter
+	io.Closer
 }
 
-func (r *ReadCloser)Read(p []byte)(int, error){
-    return r.ReadWriter.Read(p)
+func (r *ReadCloser) Read(p []byte) (int, error) {
+	return r.ReadWriter.Read(p)
 }
-func (r *ReadCloser)Close()error{
-    return r.Closer.Close()
+func (r *ReadCloser) Close() error {
+	return r.Closer.Close()
 }
 
-func (t *MyTransport)RoundTrip(req *http.Request)(*http.Response, error){
-    log.Println("++++++++++++++++++++++++")
-    log.Printf("%+v\n", req.Body)
-    req.Body = &ReadCloser{
-        ReadWriter: bandwidth.NewReader(req.Body, 10 * 1024 * 1024, time.Second),
-        Closer: req.Body,
-    }
-    return t.Transport.RoundTrip(req)
+func (t *MyTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	log.Println("++++++++++++++++++++++++")
+	log.Printf("%+v\n", req.Body)
+	if req.Body == nil {
+		return t.Transport.RoundTrip(req)
+	}
+	req.Body = &ReadCloser{
+		ReadWriter: bandwidth.NewReader(req.Body, 10*1024*1024, time.Second),
+		Closer:     req.Body,
+	}
+	return t.Transport.RoundTrip(req)
 }
 
 func main() {
@@ -57,27 +63,40 @@ func main() {
 
 	log.Println("+++++++++", 1)
 	sess, err := session.NewSession(&aws.Config{
-        Region: aws.String("ap-northeast-1"),
-        HTTPClient: &http.Client{
-            Transport: &MyTransport{
-                Transport: http.Transport{
-                    ResponseHeaderTimeout: 1 * time.Second,
-                    WriteBufferSize: 1024 * 1024,
-                },
-            },
-        },
-    })
+		Region: aws.String("ap-northeast-1"),
+		HTTPClient: &http.Client{
+			Transport: &MyTransport{
+				//Transport: http.Transport{
+				//    Proxy: http.ProxyFromEnvironment,
+				//    DialContext: (&net.Dialer{
+				//        Timeout: 30 * time.Second,
+				//        KeepAlive: 30 * time.Second,
+				//        DualStack: true,
+				//    }).DialContext,
+				//    ForceAttemptHTTP2: true,
+				//    MaxIdleConns: 100,
+				//    IdleConnTimeout: 90 * time.Second,
+				//    TLSHandshakeTimeout: 10 * time.Second,
+				//    ExpectContinueTimeout: 1 * time.Second,
+
+				//    //ResponseHeaderTimeout: 1 * time.Second,
+				//    //WriteBufferSize: 1 * 1024 * 1024,
+				//},
+			},
+		},
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println("+++++++++", 2)
-	uploader := s3manager.NewUploader(sess, func(u *s3manager.Uploader){
-        u.BufferProvider = s3manager.NewBufferedReadSeekerWriteToPool(1 * 1024 * 1024)
-        u.MaxUploadParts = 1000
-        //u.PartSize = 100 * 1024 * 1024
-        u.Concurrency = 1
-	})
+	s3Inst := s3.New(sess)
+	//uploader := s3manager.NewUploader(sess, func(u *s3manager.Uploader){
+	//    //u.BufferProvider = s3manager.NewBufferedReadSeekerWriteToPool(1 * 1024 * 1024)
+	//    //u.MaxUploadParts = 1000
+	//    ////u.PartSize = 100 * 1024 * 1024
+	//    //u.Concurrency = 1
+	//})
 
 	log.Println("+++++++++", 3)
 	fd, err := os.OpenFile(filepath, os.O_RDONLY, 0755)
@@ -89,12 +108,17 @@ func main() {
 	defer fd.Close()
 
 	log.Println("+++++++++", 5)
-	res, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String("test-rssh"),
+	res, err := s3Inst.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String("test-araumi"),
 		Key:    aws.String("giga1"),
-		//Body: bandwidth.NewReader(fd, 1 * 1024 * 1024, time.Second),
-		Body: fd,
+		Body:   fd,
 	})
+	//res, err := uploader.Upload(&s3manager.UploadInput{
+	//	Bucket: aws.String("test-rssh"),
+	//	Key:    aws.String("giga1"),
+	//	//Body: bandwidth.NewReader(fd, 1 * 1024 * 1024, time.Second),
+	//	Body: fd,
+	//})
 	if err != nil {
 		log.Fatal(err)
 	}
